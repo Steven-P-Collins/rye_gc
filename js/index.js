d1645a61266d7a7abff0c65b3de2a3e3978a8fba
Steven <stecolli2@publicisgroupe.net>
2021-03-25 14:56:11 -0400

    Updated click through for phrasing on index page

--- js/index.js
+++ js/index.js
@@ -11,11 +11,11 @@ hideImg = () => {
 
     aboutImg.onclick = () => {
         aboutImg.classList.toggle('about_hidden');
+        aboutImg.children[0].classList.toggle('about_text');
     };
 
     historyImg.onclick = () => {
         historyImg.classList.toggle('history_hidden');
+        historyImg.children[0].classList.toggle('history_text');
     };
-
-
 };
