cb269b61a80782f7fbffb9956b91c667bca2ed1c
Steven <stecolli2@publicisgroupe.net>
2021-04-16 16:43:34 -0400

    Added showing of blurbs. Chnged some imgs, fixed heros slightly

---
+++ js/site.js
@@ -0,0 +1,13 @@
+// window.onload = () => {
+//
+//     hideImg();
+//
+// };
+//
+// hideImg = () => {
+//     let left = document.getElementById('card-left'),
+//         center = document.getElementById('card-center'),
+//         right = document.getElementById('card-right');
+//
+//
+// };
